a436bd6e5b086e09c4b5688472a47d5847ac0697
Jonathan Khanlian <jdkhanlian@gmail.com>
2023-11-06 22:29:49 -0500

    changed fetch request to point to backend on render

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ function playmusic(audioPath) {
 
 async function fetchResponse(input_string) {
   try {
-    const response = await fetch(`http://127.0.0.1:8000/get-response/${input_string}`);
-    // const response = await fetch(`https://khatchig.onrender.com/get-response/${input_string}`);
+    // const response = await fetch(`http://127.0.0.1:8000/get-response/${input_string}`);
+    const response = await fetch(`https://khatchig.onrender.com/get-response/${input_string}`);
     const output = await response.json();
     console.log("output:", output);
     if (output["image_request"] != null) {
